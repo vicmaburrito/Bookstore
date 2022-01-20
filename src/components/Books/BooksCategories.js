02d187fcaf54b42a653211145e40fd293ed9a4bc
vicmaburrito <vmaldaraca@gmail.com>
2022-01-20 16:23:05 -0600

    Create book categories and set to an object imported action which will be exported

---
+++ src/components/Books/BooksCategories.js
@@ -0,0 +1,2 @@
+const categories = ['Romance', 'Crime and Thriller', 'Childrens Books', 'Religious', 'Self-help', 'Humor', 'Fantasy and Sci-fi', 'poetry'];
+export default categories;
---
+++ src/redux/books/index.js
@@ -0,0 +1,8 @@
+import { addBook, removeBook } from './books';
+
+const allActions = {
+  addBook,
+  removeBook,
+};
+
+export default allActions;
