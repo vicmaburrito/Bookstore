ced3a01934236be32a5b2b89112bd0559260d949
vicmaburrito <vmaldaraca@gmail.com>
2022-01-19 22:06:10 -0600

    Add book icon to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Bookstore</Link>
+    <nav className="navbar d-flex justify-content-between">
+      <div className="display-4 ">
+        <Link to="/" className="text-black text-decoration-none p-3">
+          Bookstore
+        </Link>
+        <FontAwesomeIcon icon={faBook} />
+      </div>
       <div>
-        <Link to="/">Books</Link>
+        <Link to="/" className="text-black text-decoration-none">Books</Link>
         <br />
-        <Link to="/categories">Categories</Link>
+        <Link to="/categories" className="text-black text-decoration-none">Categories</Link>
       </div>
     </nav>
   );
 }
+
 export default Navbar;
