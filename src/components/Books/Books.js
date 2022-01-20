e42fed708c67547c016bc434cad81ba5278d231a
vicmaburrito <vmaldaraca@gmail.com>
2022-01-19 22:38:49 -0600

    Create table instead span and fix position in categories and books components

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -2,8 +2,69 @@ import React from 'react';
 
 function Books() {
   return (
-    <div>
-      <span> &quot;Book&quot; 1 by &quot;Author1&quot;  </span>
+    <div className="d-flex justify-content-center mt-5">
+      <table>
+        <tr>
+          <th>
+            <b>
+              &quot;The Lord of the Rings&quot;
+            </b>
+          </th>
+          <th>by</th>
+          <th>&quot;J.R.R. Tolkien&quot;</th>
+          <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+        </tr>
+        <tr>
+          <th>
+            <b>
+              &quot;The Lord of the Rings&quot;
+            </b>
+          </th>
+          <th>by</th>
+          <th>&quot;J.R.R. Tolkien&quot;</th>
+          <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+        </tr>
+        <tr>
+          <th>
+            <b>
+              &quot;The Lord of the Rings&quot;
+            </b>
+          </th>
+          <th>by</th>
+          <th>&quot;J.R.R. Tolkien&quot;</th>
+          <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+        </tr>
+        <tr>
+          <th>
+            <b>
+              &quot;The Lord of the Rings&quot;
+            </b>
+          </th>
+          <th>by</th>
+          <th>&quot;J.R.R. Tolkien&quot;</th>
+          <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+        </tr>
+        <tr>
+          <th>
+            <b>
+              &quot;The Lord of the Rings&quot;
+            </b>
+          </th>
+          <th>by</th>
+          <th>&quot;J.R.R. Tolkien&quot;</th>
+          <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+        </tr>
+        <tr>
+          <th>
+            <b>
+              &quot;The Lord of the Rings&quot;
+            </b>
+          </th>
+          <th>by</th>
+          <th>&quot;J.R.R. Tolkien&quot;</th>
+          <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
+        </tr>
+      </table>
     </div>
   );
 }
--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Categories() {
   return (
-    <>
+    <div className="d-flex justify-content-center mt-5">
       <h3>
         Under construction
       </h3>
-    </>
+    </div>
   );
 }
 
