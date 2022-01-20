a454c18c644fc6dd245ee87954d884c764214f01
vicmaburrito <vmaldaraca@gmail.com>
2022-01-19 22:18:03 -0600

    Fix navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="navbar d-flex justify-content-between">
+    <nav className="p-2 navbar d-flex justify-content-between">
       <div className="display-4 ">
         <Link to="/" className="text-black text-decoration-none p-3">
           Bookstore
@@ -13,9 +14,8 @@ function Navbar() {
         <FontAwesomeIcon icon={faBook} />
       </div>
       <div>
-        <Link to="/" className="text-black text-decoration-none">Books</Link>
-        <br />
-        <Link to="/categories" className="text-black text-decoration-none">Categories</Link>
+        <Link to="/" className="mr-2 p-3 h5 text-black text-decoration-none">Books</Link>
+        <Link to="/categories" className="mr-2 p-3 h5 text-black text-decoration-none">Categories</Link>
       </div>
     </nav>
   );
