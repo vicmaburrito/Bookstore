1ab351ebf43a48120e0aed8bea031c83b8879a5e
vicmaburrito <vmaldaraca@gmail.com>
2022-01-20 22:18:10 -0600

    Add Selector to set a query to states

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import FormBook from './formBook';
+import { useSelector } from 'react-redux';
 
-function Books() {
+const Books = () => {
+  const books = useSelector((state) => state.books);
+
   return (
-    <>
-      <FormBook />
-      <div className="d-flex justify-content-center mt-5">
-        <span>Hello World!</span>
-      </div>
-    </>
+    <div className="container">
+      <ul>
+        {
+          books.map((book) => (
+            {/* console.log(book.title) */}
+          ))
+        }
+      </ul>
+    </div>
   );
-}
+};
 
 export default Books;
