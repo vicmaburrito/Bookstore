a4012f45f7c12cc780afb8171b0e1c5fa0808445
vicmaburrito <vmaldaraca@gmail.com>
2022-01-20 21:34:32 -0600

    Set layout form

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import formBook from './formBook';
+import FormBook from './formBook';
 
 function Books() {
   return (
     <>
-      <formBook />
+      <FormBook />
       <div className="d-flex justify-content-center mt-5">
-        <table>
-          <tr>
-            <th>
-              <b>
-                &quot;The Lord of the Rings&quot;
-              </b>
-            </th>
-            <th>by</th>
-            <th>&quot;J.R.R. Tolkien&quot;</th>
-            <th><button type="button" className="btn btn-outline-dark btn-sm">Remove</button></th>
-          </tr>
-        </table>
+        <span>Hello World!</span>
       </div>
     </>
   );
--- src/components/Books/formBook.js
+++ src/components/Books/formBook.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import allActions from '../../redux/books/index';
+import React from 'react';
+import '../Assets/Form.css';
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import allActions from '../../redux/books/index';
 
-const formBook = () => {
-  const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
-  const [author, setAuthor] = useState('');
-  const id = Date.now();
+function FormBook() {
+// const FormBook = () => {
+  // const [title, setTitle] = useState('');
+  // const [category, setCategory] = useState('');
+  // const [author, setAuthor] = useState('');
+  // const id = Date.now();
 
-  const book = {
-    id, title, author, category,
-  };
+  // const book = {
+  //   id, title, author, category,
+  // };
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  dispatch(allActions.addBook(book));
-  setTitle('');
-  setCategory('');
-  setAuthor('');
+  // dispatch(allActions.addBook(book));
+  // setTitle('');
+  // setCategory('');
+  // setAuthor('');
 
   return (
     <div className="d-flex justify-content-center mt-5">
-      <form>
+      <form className="d-flex flex-column my-5">
         <h2>Add a Book</h2>
-        <input className="form-control" type="text" placeholder="Write a book" />
-        <input className="form-control" type="text" placeholder="Write a Authot" />
+        <input className="my-2 inp-st" type="text" placeholder="Write a book" />
+        <input className="my-2 inp-st" type="text" placeholder="Write a Author" />
+        <input type="submit" className="btn-form align-self-end" value="Submit" />
       </form>
     </div>
   );
-};
+}
 
-export default formBook;
+export default FormBook;
