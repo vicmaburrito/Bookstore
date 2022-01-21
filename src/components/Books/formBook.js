c8c9d1b7f8a28750f3f4e348ac95ccb2a24ae927
vicmaburrito <vmaldaraca@gmail.com>
2022-01-20 21:45:12 -0600

    Add event to form

--- src/components/Books/formBook.js
+++ src/components/Books/formBook.js
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import allActions from '../../redux/books/index';
 import '../Assets/Form.css';
-// import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import allActions from '../../redux/books/index';
 
-function FormBook() {
-// const FormBook = () => {
-  // const [title, setTitle] = useState('');
-  // const [category, setCategory] = useState('');
-  // const [author, setAuthor] = useState('');
-  // const id = Date.now();
+const FormBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [author, setAuthor] = useState('');
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const id = Date.now();
 
-  // const book = {
-  //   id, title, author, category,
-  // };
+    const book = {
+      id, title, author, category,
+    };
 
-  // const dispatch = useDispatch();
-
-  // dispatch(allActions.addBook(book));
-  // setTitle('');
-  // setCategory('');
-  // setAuthor('');
-
+    dispatch(allActions.addBook(book));
+    setTitle('');
+    setCategory('');
+    setAuthor('');
+  };
   return (
     <div className="d-flex justify-content-center mt-5">
-      <form className="d-flex flex-column my-5">
+      <form className="d-flex flex-column my-5" onSubmit={submitBookToStore}>
         <h2>Add a Book</h2>
         <input className="my-2 inp-st" type="text" placeholder="Write a book" />
         <input className="my-2 inp-st" type="text" placeholder="Write a Author" />
@@ -32,6 +31,6 @@ function FormBook() {
       </form>
     </div>
   );
-}
+};
 
 export default FormBook;
