d1fd9296573fba0b03a071ab48b6563cf52fa11a
vicmaburrito <vmaldaraca@gmail.com>
2022-01-21 19:11:50 -0600

    Set navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Assets/Navbar.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <nav className="p-2 navbar d-flex justify-content-between">
       <div className="display-4 ">
-        <Link to="/" className="title-navbar text-blue text-decoration-none">
+        <Link to="/" className="title-navbar text-decoration-none">
           Bookstore CMS
         </Link>
+        <Link to="/" className="navbar-items text-decoration-none">Books</Link>
+        <Link to="/categories" className="navbar-items text-decoration-none">Categories</Link>
       </div>
-      <div>
-        <Link to="/" className="mr-2 p-3 h5 text-black text-decoration-none">Books</Link>
-        <Link to="/categories" className="mr-2 p-3 h5 text-black text-decoration-none">Categories</Link>
+      <div className="Oval">
+        <FontAwesomeIcon icon={faUser} className="" />
       </div>
     </nav>
   );
