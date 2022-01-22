2e8291c1e2854a5355dfc828baf3b7d9ac4f10f8
vicmaburrito <vmaldaraca@gmail.com>
2022-01-21 21:55:08 -0600

    Fix and sort classes of form

--- src/components/Books/formBook.js
+++ src/components/Books/formBook.js
@@ -27,19 +27,20 @@ const FormBook = () => {
     <>
       <div className="line" />
       <div className="form-container">
-        <form onSubmit={submitBookToStore}>
+        <form className="" onSubmit={submitBookToStore}>
           <p className="form-title">ADD NEW BOOK</p>
           <input className="input-title" type="text" placeholder="Write a book" onChange={(e) => setTitle(e.target.value)} value={title} />
-          <select className="select-category" onChange={(e) => setCategory(e.target.value)} value={category}>
-            <option>Category</option>
+          <select required className="select-category" onChange={(e) => setCategory(e.target.value)} value={category}>
+            <option value="" disabled selected hidden>Category</option>
             {
               categories.map((category) => (
                 <option key={category} value={category}>{category}</option>
               ))
             }
           </select>
-          <input className="my-2 inp-st" type="text" placeholder="Write a Author" onChange={(e) => setAuthor(e.target.value)} value={author} />
-          <input type="submit" className="btn-form align-self-end" value="Submit" />
+          {/* <input className="my-2 inp-st" type="text" placeholder="Write a Author"
+          onChange={(e) => setAuthor(e.target.value)} value={author} /> */}
+          <button type="button" className="btn-submit" value="Submit">ADD BOOK</button>
         </form>
       </div>
     </>
