e7bb4f2efac2183122bd59fe192b181f2a0c7026
vicmaburrito <vmaldaraca@gmail.com>
2022-01-21 18:53:44 -0600

    Set title style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook } from '@fortawesome/free-solid-svg-icons';
+import '../Assets/Navbar.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <nav className="p-2 navbar d-flex justify-content-between">
       <div className="display-4 ">
-        <Link to="/" className="text-black text-decoration-none p-3">
-          Bookstore
+        <Link to="/" className="title-navbar text-blue text-decoration-none">
+          Bookstore CMS
         </Link>
-        <FontAwesomeIcon icon={faBook} />
       </div>
       <div>
         <Link to="/" className="mr-2 p-3 h5 text-black text-decoration-none">Books</Link>
